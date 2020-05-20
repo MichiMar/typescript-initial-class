cf292d71b2a624195cc5d563bad0ced714539408
MichiMar <michimar.alexis@gmail.com>
2020-05-19 23:05:30 -0500

    learnig typescript 2

--- helloworld.ts
+++ helloworld.ts
@@ -1,3 +1,10 @@
 var hola: string = "hola mundo";
+var numero = 2;
+
+// typescript me lanca un warning por que sabe que no puedo asignar un string a la variable que ya declare numerica antes
+// numero = "ds";
+
+numero = 24;
 
 console.log(hola);
+console.log(numero);
