ec918878161ed5b0adf8c4b3d2573eff000ed404
MichiMar <michimar.alexis@gmail.com>
2020-05-27 23:49:41 -0500

    declarong arrays

--- tipos.ts
+++ tipos.ts
@@ -1,5 +1,14 @@
+// estos son los tipos basicos
+
 var cuenta: string = "cuenta";
 var cuentaNumerica: number = 2000;
 var cuentaActiva: boolean = false;
+
+// tambien tenemos arreglos (Arrays)
+// para dejar declarado que mi datopuede ser de cualquier tipo solo debo declararlo como any)
+
+var cuentas: any[] = [2, "cuenta2", false];
+
+cuentas[0];
 
 console.log(cuenta);
