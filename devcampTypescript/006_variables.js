4c9f67596d971f3d527654f03724066214d82203
MichiMar <michimar.alexis@gmail.com>
2020-06-03 21:35:48 -0500

    diferent ways using variables in typescript

--- devcampTypescript/006_variables.js
+++ devcampTypescript/006_variables.js
@@ -1,6 +1,22 @@
-var fullName = "Michelle MArquez";
-var paidAccount = true;
-var versionNumber = 1.3;
-console.log(fullName);
-console.log(paidAccount);
-console.log(versionNumber);
+// var fullName: string = "Michelle MArquez";
+// let paidAccount: boolean = true;
+// const versionNumber: number = 1.3;
+// fullName = "Momo";
+// paidAccount = false;
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+function printName(f, l) {
+    //   var greeting: string = "Hi there, ";
+    //   console.log(greeting + f + " " + l);
+    //   var greeting: string = "Hey there, ";
+    //   console.log(greeting + f + " " + l);
+    //there is any error here
+    var greeting = "Hi there, ";
+    console.log(greeting + f + " " + l);
+    greeting = "Hey there, ";
+    console.log(greeting + f + " " + l);
+    //  there is an error, I just can change the declaration
+}
+printName("Mich", "Soto");
+//# sourceMappingURL=006_variables.js.map
--- devcampTypescript/006_variables.ts
+++ devcampTypescript/006_variables.ts
@@ -1,10 +1,28 @@
-var fullName: string = "Michelle MArquez";
-let paidAccount: boolean = true;
-const versionNumber: number = 1.3;
+// var fullName: string = "Michelle MArquez";
+// let paidAccount: boolean = true;
+// const versionNumber: number = 1.3;
 
-fullName = "Momo";
-paidAccount = false;
+// fullName = "Momo";
+// paidAccount = false;
 
-console.log(fullName);
-console.log(paidAccount);
-console.log(versionNumber);
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+
+function printName(f, l) {
+  //   var greeting: string = "Hi there, ";
+  //   console.log(greeting + f + " " + l);
+
+  //   var greeting: string = "Hey there, ";
+  //   console.log(greeting + f + " " + l);
+  //there is any error here
+
+  let greeting: string = "Hi there, ";
+  console.log(greeting + f + " " + l);
+
+  greeting = "Hey there, ";
+  console.log(greeting + f + " " + l);
+  //  there is an error, I just can change the declaration
+}
+
+printName("Mich", "Soto");
