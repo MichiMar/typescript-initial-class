da6aba813dad7acd7d0d075b97fd3fa4114f6f4d
MichiMar <michimar.alexis@gmail.com>
2020-06-10 13:44:03 -0500

    Union types

---
+++ devcampTypescript/009_union_types.js
@@ -0,0 +1,10 @@
+var players = ["Altuve", "Correra", "Bregman"];
+var players_number = [25, 3, 2];
+console.log(players);
+console.log(players_number);
+var names;
+names = ["Mar", "Betty", "Gian"];
+console.log(names);
+names = "JeongJeong";
+console.log(names);
+//# sourceMappingURL=009_union_types.js.map
---
+++ devcampTypescript/009_union_types.ts
@@ -0,0 +1,11 @@
+type PlayerArray = Array<string | number>;
+let players: PlayerArray = ["Altuve", "Correra", "Bregman"];
+let players_number: PlayerArray = [25, 3, 2];
+console.log(players);
+console.log(players_number);
+
+var names: string[] | string;
+names = ["Mar", "Betty", "Gian"];
+console.log(names);
+names = "JeongJeong";
+console.log(names);
