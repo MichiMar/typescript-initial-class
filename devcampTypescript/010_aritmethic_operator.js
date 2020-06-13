1b2f230498e417c284a7b2bc61cf1b3694be13b1
MichiMar <michimar.alexis@gmail.com>
2020-06-12 22:47:04 -0500

    arithmetic operator

---
+++ devcampTypescript/010_aritmethic_operator.js
@@ -0,0 +1,35 @@
+// +, -, *, /, %, ++, --
+var numOne = 1;
+var numTwo = 2;
+// Addition
+console.log("Addition");
+console.log(numOne + numTwo);
+// Substraction
+console.log("Substraction");
+console.log(numOne - numTwo);
+// Multiplication
+console.log("Multiplication");
+console.log(numOne * numTwo);
+// Division
+console.log("Division");
+console.log(numOne / numTwo);
+// Modulus
+console.log("Modulus:");
+var numThree = 2;
+var numFour = 21;
+console.log(numFour % numThree);
+// Increment
+console.log("Increment:");
+var x = 0;
+while (x < 10) {
+    console.log(x);
+    x++;
+}
+// Decrement
+console.log("Dencrement:");
+var x = 10;
+while (x > 0) {
+    console.log(x);
+    x--;
+}
+//# sourceMappingURL=010_aritmethic_operator.js.map
---
+++ devcampTypescript/010_aritmethic_operator.ts
@@ -0,0 +1,42 @@
+// +, -, *, /, %, ++, --
+
+var numOne: number = 1;
+var numTwo: number = 2;
+
+// Addition
+console.log("Addition");
+console.log(numOne + numTwo);
+
+// Substraction
+console.log("Substraction");
+console.log(numOne - numTwo);
+
+// Multiplication
+console.log("Multiplication");
+console.log(numOne * numTwo);
+
+// Division
+console.log("Division");
+console.log(numOne / numTwo);
+
+// Modulus
+console.log("Modulus:");
+var numThree: number = 2;
+var numFour: number = 21;
+console.log(numFour % numThree);
+
+// Increment
+console.log("Increment:");
+var x: number = 0;
+while (x < 10) {
+  console.log(x);
+  x++;
+}
+
+// Decrement
+console.log("Dencrement:");
+var x: number = 10;
+while (x > 0) {
+  console.log(x);
+  x--;
+}
