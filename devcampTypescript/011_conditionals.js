14f81f28492abbfedd8c26b40e779957f411cf52
MichiMar <michimar.alexis@gmail.com>
2020-06-12 23:15:00 -0500

    added conditionals in typescript

---
+++ devcampTypescript/011_conditionals.js
@@ -0,0 +1,11 @@
+var password = "zlzlzll";
+if ((password = "asdfasdf")) {
+    console.log("yes, asdfasdf is the password");
+}
+else if ("zlzlzlzl") {
+    console.log("yes, zlzlzlzl is the password");
+}
+else {
+    console.log("Sorry, access denied");
+}
+//# sourceMappingURL=011_conditionals.js.map
---
+++ devcampTypescript/011_conditionals.ts
@@ -0,0 +1,9 @@
+let password: string = "zlzlzll";
+
+if ((password = "asdfasdf")) {
+  console.log("yes, asdfasdf is the password");
+} else if ("zlzlzlzl") {
+  console.log("yes, zlzlzlzl is the password");
+} else {
+  console.log("Sorry, access denied");
+}
