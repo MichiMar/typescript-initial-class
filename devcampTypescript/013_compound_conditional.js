bfba7d4d69c4c9fd75fb9ad7b801e9668def86d4
MichiMar <michimar.alexis@gmail.com>
2020-06-19 23:46:45 -0500

    initial compound conditionals

---
+++ devcampTypescript/013_compound_conditional.js
@@ -0,0 +1,8 @@
+var password = "asdasd";
+if (password == "asdasd" && password == "qweqwe") {
+    console.log("Password is correct");
+}
+else {
+    console.log("Permission denied");
+}
+//# sourceMappingURL=013_compound_conditional.js.map
---
+++ devcampTypescript/013_compound_conditional.ts
@@ -0,0 +1,7 @@
+let password: string = "asdasd";
+
+if (password == "asdasd" && password == "qweqwe") {
+  console.log("Password is correct");
+} else {
+  console.log("Permission denied");
+}
