ae18b410faa18cecb83274dde03ab0b22c6e014b
MichiMar <michimar.alexis@gmail.com>
2020-07-02 13:36:12 -0500

    fixed errors

--- devcampTypescript/015_functions.js
+++ devcampTypescript/015_functions.js
@@ -3,17 +3,21 @@
 // }
 // console.log(fullName("first", "last"));
 function gradeGenerator(grade) {
-  if (grade < 60) {
-    return "F";
-  } else if (grade >= 60 && grade < 70) {
-    return "D";
-  } else if (grade >= 70 && grade < 80) {
-    return "C";
-  } else if (grade >= 80 && grade < 90) {
-    return "B";
-  } else {
-    return "A";
-  }
+    if (grade < 60) {
+        return "F";
+    }
+    else if (grade >= 60 && grade < 70) {
+        return "D";
+    }
+    else if (grade >= 70 && grade < 80) {
+        return "C";
+    }
+    else if (grade >= 80 && grade < 90) {
+        return "B";
+    }
+    else {
+        return "A";
+    }
 }
 console.log(gradeGenerator(45));
 console.log(gradeGenerator(100));
