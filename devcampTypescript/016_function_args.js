70d80b5ab5376c341d6941e71089a35ead3c3cf7
MichiMar <michimar.alexis@gmail.com>
2020-07-02 13:17:42 -0500

    arguments an darray args

--- devcampTypescript/016_function_args.js
+++ devcampTypescript/016_function_args.js
@@ -1,14 +1,27 @@
-// ? = this argument is optional
-// '=' default arg
-function printAddress(street, streetTwo, state) {
-    if (state === void 0) { state = "AZ"; }
-    console.log(street);
-    if (streetTwo) {
-        console.log(streetTwo);
+// // ? = this argument is optional
+// // '=' default arg
+// // you can not have a requier value after an optional (?)
+// // you can have the default value (=) anywhere
+// function printAddress(street: string, streetTwo?: string, state = "AZ") {
+//   console.log(street);
+//   if (streetTwo) {
+//     console.log(streetTwo);
+//   }
+//   console.log(state);
+// }
+// printAddress("12 Any St");
+// printAddress("123 Any St", "Suit 540");
+// printAddress("123 Any St", "Suit 540", "UT");
+function lineupCard(team) {
+    var players = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        players[_i - 1] = arguments[_i];
     }
-    console.log(state);
+    console.log("Team: " + team);
+    for (var _a = 0, players_1 = players; _a < players_1.length; _a++) {
+        var player = players_1[_a];
+        console.log(player);
+    }
 }
-printAddress("12 Any St");
-printAddress("123 Any St", "Suit 540");
-printAddress("123 Any St", "Suit 540", "UT");
+lineupCard("Astros", "ALtuve", "Pimienta", "Chabelo");
 //# sourceMappingURL=016_function_args.js.map
--- devcampTypescript/016_function_args.ts
+++ devcampTypescript/016_function_args.ts
@@ -1,13 +1,24 @@
-// ? = this argument is optional
-// '=' default arg
-function printAddress(street: string, streetTwo?: string, state = "AZ") {
-  console.log(street);
-  if (streetTwo) {
-    console.log(streetTwo);
+// // ? = this argument is optional
+// // '=' default arg
+// // you can not have a requier value after an optional (?)
+// // you can have the default value (=) anywhere
+// function printAddress(street: string, streetTwo?: string, state = "AZ") {
+//   console.log(street);
+//   if (streetTwo) {
+//     console.log(streetTwo);
+//   }
+//   console.log(state);
+// }
+
+// printAddress("12 Any St");
+// printAddress("123 Any St", "Suit 540");
+// printAddress("123 Any St", "Suit 540", "UT");
+
+function lineupCard(team: string, ...players: string[]) {
+  console.log("Team: " + team);
+  for (let player of players) {
+    console.log(player);
   }
-  console.log(state);
 }
 
-printAddress("12 Any St");
-printAddress("123 Any St", "Suit 540");
-printAddress("123 Any St", "Suit 540", "UT");
+lineupCard("Astros", "ALtuve", "Pimienta", "Chabelo");
