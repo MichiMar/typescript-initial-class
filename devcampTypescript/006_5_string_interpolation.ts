5e5c8479f395d3be27caa862b10e432ce9bee3b1
Mich Marquez <michimar.alexis@gmail.com>
2021-05-01 23:38:56 -0500

    comentarios añadidos

--- devcampTypescript/006_5_string_interpolation.ts
+++ devcampTypescript/006_5_string_interpolation.ts
@@ -2,3 +2,6 @@
 var msg: string = "Mich";
 console.log("A long message to " + msg + " filed with text");
 console.log(`A long message to ${msg} filed with text`);
+
+// la declaracion de la variable se establece antes del valos de la variable,
+// y la interpolacoion se realiza de la misma manera que en JS
